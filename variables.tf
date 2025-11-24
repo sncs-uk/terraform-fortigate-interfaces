59c65b275acb7198213db757eb0e857336391a13
Richard Franks <richard.franks@securenetcoms.co.uk>
2025-11-24 13:49:18 +0000

    Added version constraints

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
-variable config_path {
+variable "config_path" {
   description = "Path to base configuration directory"
   type        = string
 }
 
-variable interface_key {
+variable "interface_key" {
   description = "Key within the configuration path to use for the interface configuration"
   type        = string
   default     = "interfaces"
